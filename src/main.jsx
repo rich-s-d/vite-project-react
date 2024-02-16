8223ca338c7e6a0fcb9d06174c3b4b1ba1eda16d
Shane Rich <shanerich@Shanes-MacBook-Pro.local>
2024-02-16 11:11:22 +0100

    Added Chakra2 UI components to project.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App2 from './App2.jsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <ChakraProvider>
+      <App2 />
+    </ChakraProvider>
   </React.StrictMode>,
 )
